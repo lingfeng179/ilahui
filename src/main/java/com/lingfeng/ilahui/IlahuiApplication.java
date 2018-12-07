f99da13014b30c879c9891e3faa30efe06c1a783
lingfeng179 <junjielingfeng179@163.com>
2018-12-07 22:13:51 +0800

    IDEA GITHUB

---
+++ src/main/java/com/lingfeng/ilahui/IlahuiApplication.java
@@ -0,0 +1,12 @@
+package com.lingfeng.ilahui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlahuiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IlahuiApplication.class, args);
+    }
+}
